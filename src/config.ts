54af8167dff70e26105f79371a5a7c64007a798d
yoon4027 <yooooonus@gmail.com>
2022-04-17 22:23:05 +0500

    Breaking changes

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const domain = "http://localhost:9012";
 export const Oauth2 =
-  "https://discord.com/api/oauth2/authorize?client_id=965211726817660978&redirect_uri=https%3A%2F%2Fmxty.mirodash.xyz%2Fcallback&response_type=code&scope=identify";
-export const callback = "https://mxty.mirodash.xyz/callback";
+  "https://discord.com/api/oauth2/authorize?client_id=965211726817660978&redirect_uri=http%3A%2F%2Flocalhost%3A9012%2Fcallback&response_type=code&scope=identify";
+export const callback = "http://localhost:9012/callback";
 export const secret = "eb085dd8-a52d-4b6b-b72d-ff4d55f5eb08";
 export const client = {
   id: "965211726817660978",
-  secret: "kGQQNKRNvGwMcLzEj19Pe-rxL9ulHG7L",
+  secret: "Zh-3aJZoIxl4OYD-GCZof6f_HJosqTzy",
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import fastify, { FastifyRequest } from "fastify";
 import fastifyCookie from "fastify-cookie";
-import { callback, client, domain, Oauth2, secret } from "./config";
+import { callback, client, Oauth2, secret } from "./config";
 
 const app = fastify({ logger: true });
 
@@ -15,7 +15,7 @@ app.register(fastifyCookie, {
 });
 
 app.get("/login", async (req, reply) => {
-  if (isAuthenticated(req)) return await reply.redirect(200, `${domain}/info`);
+  if (isAuthenticated(req)) return await reply.redirect(`/info`);
 
   return await reply.redirect(Oauth2);
 });
@@ -23,40 +23,45 @@ app.get("/login", async (req, reply) => {
 app.get("/logout", async (req, res) => {
   if (!isAuthenticated(req))
     return await res.status(401).send({ message: "Not Authorized" });
-  await res.clearCookie("token");
-  return await res.redirect(`${domain}/login`);
+
+  res.clearCookie("token");
+  return await res.redirect(`/login`);
 });
 
-app.get("/callback", async (req, reply) => {
-  const { code } = req.query as any;
+app.get(
+  "/callback",
+  { schema: { querystring: { code: { type: "string" } } } },
+  async (req, reply) => {
+    const code = (req.query as any).code as string;
 
-  if (!code)
-    return await reply.status(404).send({ message: "No code provided." });
+    if (!code)
+      return await reply.status(404).send({ message: "No code provided." });
 
-  try {
-    const data = await axios.post(
-      "https://discord.com/api/v10/oauth2/token",
-      new URLSearchParams({
-        client_id: client.id,
-        client_secret: client.secret,
-        grant_type: "authorization_code",
-        code,
-        redirect_uri: callback,
-      }),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
+    try {
+      const data = await axios.post(
+        "https://discord.com/api/v10/oauth2/token",
+        new URLSearchParams({
+          client_id: client.id,
+          client_secret: client.secret,
+          grant_type: "authorization_code",
+          code: code.toString(),
+          redirect_uri: callback,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
 
-    await reply.setCookie("token", data.data.access_token);
-    return await reply.status(200).redirect(`${domain}/info`);
-  } catch (e) {
-    console.error(e);
-    return await reply.status(404).send({ message: "An error has occoured" });
+      reply.setCookie("token", data.data.access_token);
+      return reply.redirect(`/info`);
+    } catch (e) {
+      console.error(e);
+      return await reply.status(404).send({ message: "An error has occoured" });
+    }
   }
-});
+);
 
 app.get("/info", async (req, res) => {
   if (!isAuthenticated(req))
